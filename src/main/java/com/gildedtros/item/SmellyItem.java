85f5becb31fd5f241755e6d44af214ec23b58e94
rokeppen <robinkep@gmail.com>
2024-08-22 10:57:37 +0200

    Add smelly items

--- src/main/java/com/gildedtros/item/SmellyItem.java
+++ src/main/java/com/gildedtros/item/SmellyItem.java
@@ -2,6 +2,9 @@ package com.gildedtros.item;
 
 import com.gildedtros.Item;
 
+/**
+ * Smelly items degrade in quality twice as fast as normal items.
+ */
 public class SmellyItem extends DecoratedItem {
 
     public SmellyItem(Item item) {
